c1c5675e0e330b0486d32c8166baf7b980e4746a
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-16 14:29:54 +0500

    commit

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 # echo-client.py
-
+import dearpygui.dearpygui as dpg
 import socket
 
 def response(host,port,data):
@@ -12,14 +12,23 @@ def response(host,port,data):
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-import dearpygui.dearpygui as dpg
+
+
+
+
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
+dpg.create_viewport(title='Client', width=600, height=300)
+
+
+def upgate(a):
+    tm = str(response(HOST, PORT, b"get_time"))
+    dpg.set_value(a, tm)
 
 with dpg.window(label="Example Window"):
     dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
+    a = dpg.last_item()
+    dpg.add_button(label="Get_data", callback=lambda m, s:upgate(a) )
     dpg.add_input_text(label="string", default_value="Quick brown fox")
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
 
