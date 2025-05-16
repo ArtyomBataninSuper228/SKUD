5dd68fa50d5da74b8437876fbca2cb9dcc6f8a57
kozik <kozik2008@inbox.ru>
2025-05-16 16:31:34 +0500

    commit

--- client.py
+++ client.py
@@ -37,10 +37,21 @@ dpg.bind_font(font_domino)
 def upgate(a):
     tm = str(response(HOST, PORT, b"get_time"))
     dpg.set_value(a, tm)
+
+def cameras_callback(sender, data):
+    with dpg.window(width=300, height=300):
+        dpg.add_text(data)
+        id_w = dpg.last_item()
 def doors_callback(sender, data):
     with dpg.window(width=300, height=300):
-        dpg.add_text()
+        dpg.add_text(data)
         id_w = dpg.last_item()
+def sensors_callback(sender, data):
+    with dpg.window(width=300, height=300):
+        dpg.add_text(data)
+        id_w = dpg.last_item()
+
+
 cameras = response(HOST, PORT, b"get_cameras").decode("utf-8").split("\n")
 doors = response(HOST, PORT, b"get_doors").decode("utf-8").split("\n")
 sensors = response(HOST, PORT, b"get_sensors").decode("utf-8").split("\n")
@@ -54,9 +65,9 @@ with dpg.window(width = int(screeninfo.get_monitors()[0].width),height = int(scr
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
     #1
     with dpg.group(horizontal=True):
-        dpg.add_listbox(cameras,num_items=15,width=400,label="Tabs",callback=lambda m, n:upgate(id_e))
+        dpg.add_listbox(cameras,num_items=15,width=400,label="Tabs",callback=lambda m, s:cameras_callback(m,s))
         dpg.add_listbox(doors, num_items=15,width=400, label="Tabs",callback=lambda m, s:doors_callback(m,s))
-        dpg.add_listbox(sensors, num_items=15,width=400, label="Tabs",callback=lambda m, s:upgate(id_e))
+        dpg.add_listbox(sensors, num_items=15,width=400, label="Tabs",callback=lambda m, s:sensors_callback(m,s))
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
