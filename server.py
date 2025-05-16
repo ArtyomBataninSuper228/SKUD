435f86cd5b1a9b38545c451257ab23e1b5313ffc
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-16 12:43:39 +0500

    commit

--- server.py
+++ server.py
@@ -10,9 +10,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
+            print(conn)
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
-
                 conn.sendall(bytes("Hello!", "utf-8"))
+                c
