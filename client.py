30604fc7f714a6c533db436923eae782e46b2452
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-16 14:24:32 +0500

    commit

--- client.py
+++ client.py
@@ -12,5 +12,18 @@ def response(host,port,data):
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
+import dearpygui.dearpygui as dpg
 
-print(f"Received {response(HOST,PORT,b'get_time')!r}")
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Save")
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
