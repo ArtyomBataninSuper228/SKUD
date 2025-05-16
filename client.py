ab79ed356515f31e540515bcf94518768d0a319f
kozik <kozik2008@inbox.ru>
2025-05-16 12:40:20 +0500

    commit

--- client.py
+++ client.py
@@ -2,12 +2,15 @@
 
 import socket
 
+def response(host,port,data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(data)
+        resp = s.recv(1024)
+        return resp
+
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
 
-print(f"Received {data!r}")
+print(f"Received {response(HOST,PORT,b'01011')!r}")
