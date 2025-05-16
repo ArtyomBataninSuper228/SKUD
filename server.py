38a359f0ef11e76720e151b97bc8cf790029202e
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-16 12:30:09 +0500

    commit

--- server.py
+++ server.py
@@ -15,4 +15,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 data = conn.recv(1024)
                 if not data:
                     break
+
                 conn.sendall(bytes("Hello!", "utf-8"))
