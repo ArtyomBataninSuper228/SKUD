e4348052d01ae8ab90ab57cbda7c59209151ec08
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-16 13:37:36 +0500

    commit

--- SKUD.py
+++ SKUD.py
@@ -1,5 +1,5 @@
 from PTZ import *
-
+import socket
 class camera:
     def __init__(self, ip, location, name, port):
         self.ip = ip
@@ -144,8 +144,18 @@ class camera:
                 ay = 0
                 # Read the input image
             cv2.destroyAllWindows()
+
 class door:
-    def __init__(self, name, location, is_opened, ):
+    def __init__(self, name, location, is_opened, ip, level, port):
         self.name = name
         self.location = location
         self.is_opened = is_opened
+        self.ip = ip
+        self.level = level
+        self.port = port
+    def open(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.ip, self.port))
+            s.sendall(b"Open the door")
+            resp = s.recv(1024)
+            return resp
