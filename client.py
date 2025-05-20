98ea0a9103aa02bee13583a365aeef70abaf98cb
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-20 16:34:35 +0500

    commit

--- client.py
+++ client.py
@@ -80,15 +80,18 @@ dpg.create_viewport(x_pos=0,y_pos=0,title='Client',width = int(screeninfo.get_mo
 
 
 
-
+### Регистрация шрифтов
 text_size = int(settings["textsz"])
-
 with dpg.font_registry():
     with dpg.font("Domino Italic.otf", text_size) as font_domino:
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
     with dpg.font("Domino Italic.otf", text_size + 10) as big_font_domino:
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
 dpg.bind_font(font_domino)
+
+
+
+
 
 def upgate(a):
     tm = str(response(HOST, PORT, b"get_time"))
@@ -128,7 +131,7 @@ def doors_callback(sender, data):
                 level = dpg.get_value(id - 3)
                 ip = dpg.get_value(id - 2)
                 port = dpg.get_value(id - 1)
-                response(HOST, PORT, bytes(f'add_camera\n{ip}\n{locate}\n{level}\n{name}\n{port}', 'utf8'))
+                response(HOST, PORT, bytes(f'add_door\n{ip}\n{locate}\n{level}\n{name}\n{port}', 'utf8'))
 
 
             dpg.add_input_text(hint='Введите название')
@@ -156,7 +159,7 @@ def sensors_callback(sender, data):
                 locate = dpg.get_value(id - 3)
                 ip = dpg.get_value(id - 2)
                 port = dpg.get_value(id - 1)
-                response(HOST, PORT, bytes(f'add_camera\n{ip}\n{locate}\n{name}\n{port}', 'utf8'))
+                response(HOST, PORT, bytes(f'add_sensor\n{ip}\n{locate}\n{name}\n{port}', 'utf8'))
 
             dpg.add_input_text(hint='Введите название')
             dpg.add_input_text(hint='Введите расположение')
@@ -206,17 +209,39 @@ def updating_units():
         time.sleep(0.5)
 
 
+def open_settings():
+    with dpg.window():
+        start_tag = dpg.last_item()
+        red = 255
+        green = 255
+        blue = 0
+        d = 255/len(settings.keys())
+        for key in settings.keys():
+            with dpg.group(horizontal=True):
+                dpg.add_text(key, color=(red, green, blue))
+                dpg.add_input_text(default_value=settings[key])
+                blue += d
+                red -= d
+        def update_set():
+            global settings
+            ln = len(settings.keys())
+            settings = {}
+            for i in range(ln):
+                key = dpg.get_value(start_tag + i*3 + 2)
+                value = dpg.get_value(start_tag + i*3 + 3)
+                settings[key] = value
+            save_settings()
+        dpg.add_button(label="Сохранить", callback=update_set)
+
+
 
 
 
 with dpg.window(width = int(screeninfo.get_monitors()[0].width),height = int(screeninfo.get_monitors()[0].height), no_title_bar=True, pos=(0, 0), no_move=True, no_resize=True, tag="Window", no_collapse= True, no_bring_to_front_on_focus=True):
-    with dpg.group(horizontal=True):
-        dpg.add_text("Hello, world")
-        id_e = dpg.last_item()
-        dpg.add_button(label="Get_data", callback=lambda m, s:upgate(id_e) )
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
-
+    with dpg.menu_bar():
+        dpg.add_menu_item(label = "Настройки",callback=lambda a, s:open_settings())
+        dpg.add_menu_item(label="Просмотреть статистику")
+        dpg.add_menu_item(label="Сменить пользователя")
     with dpg.group(horizontal=True):
         dpg.add_listbox(cameras,num_items=15,width=400,callback=lambda m, s:cameras_callback(m,s), tag = "cameras")
         dpg.add_listbox(doors, num_items=15,width=400,callback=lambda m, s:doors_callback(m,s), tag = "doors")
