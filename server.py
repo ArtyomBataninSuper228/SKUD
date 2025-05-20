770e302263fb8a6e3ffc84225fd6bae2e9d332e1
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-20 20:25:18 +0500

    commit

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 
 
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = "192.168.1.252"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 import datetime
 from SKUD import *
