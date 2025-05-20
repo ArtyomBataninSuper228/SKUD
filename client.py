731e1be3ce27cd0d21ea3622a6aead05be0d8d49
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-05-20 18:48:34 +0500

    commit

--- client.py
+++ client.py
@@ -324,6 +324,20 @@ with dpg.window(width = int(screeninfo.get_monitors()[0].width),height = int(scr
     with dpg.window(label="Image"):
         dpg.add_image("texture_tag")
 
+with dpg.theme() as global_theme:
+
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+    with dpg.theme_component(dpg.mvInputInt):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (140, 255, 23), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+dpg.bind_theme(global_theme)
+
+dpg.show_style_editor()
+
 
 
 
